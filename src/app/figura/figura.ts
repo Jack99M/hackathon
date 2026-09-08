8ca154ad417b65d0543fdb575c192db5c4f6f85d
Jack99M <maytajuan314@gmail.com>
2026-09-08 17:43:29 -0400

    creacion de componente de la figura

---
+++ src/app/figura/figura.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-figura',
+  styleUrl: './figura.scss',
+  templateUrl: './figura.html',
+})
+export class Figura {}
