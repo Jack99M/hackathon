bf5b583029c0d1d19d1dd75c92401ae8d6503d16
xXSCLAXx <sevicheqweasd@gmail.com>
2026-09-08 18:15:35 -0400

    se muestra movimiento debajo de la figura

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Figura } from './figura/figura';
+import { MovimientoComponent } from './movimiento/movimiento';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Figura],
+  imports: [RouterOutlet, Figura, MovimientoComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
